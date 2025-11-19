fcc5b4eab7b224070d17bf30252cbc272812180a
Paul <paul.olukayode.pro@gmail.com>
2025-11-19 17:42:58 +0100

    feat: push to device

--- lib/chat/chat_resume_signal.ex
+++ lib/chat/chat_resume_signal.ex
@@ -1,24 +1,28 @@
 defmodule Chat.ResumeSignal do
   alias Queue.Injection
   alias Until.UniPosTime
+  alias ThrowMessageSchema
 
   @partition_id 1
 
   def resume(%Chat.ResumeStruct{
         from: %{eid: from_eid, connection_resource_id: device_id},
-        to: %{eid: to_eid}
+        to: %{eid: to_eid},
       } = _payload) do
 
     queue_id = "#{from_eid}_#{to_eid}"
 
     case Injection.fetch_messages(queue_id, device_id, @partition_id) do
       {:ok, %{messages: [message]}} ->
-        message
-        |> set_message_fields()
-        |> Map.put(:signal_offset_state, false)
-        |> Map.put(:timestamp, UniPosTime.uni_pos_time())
-        |> then(&IO.inspect(&1))
 
+        msg =
+          message
+          |> set_message_fields()
+          |> Map.put(:signal_offset_state, false)
+          |> Map.put(:timestamp, UniPosTime.uni_pos_time())
+          |> ThrowMessageSchema.build_message()
+
+        IO.inspect(msg)
 
       {:error, reason} ->
         IO.puts("Failed to fetch messages: #{inspect(reason)}")
@@ -42,6 +46,7 @@ defmodule Chat.ResumeSignal do
       }) do
 
     signal_type = if eid == from.eid, do: 2, else: 3
+    queue_id = "#{from.eid}_#{to.eid}"
 
     %{
       id: id,
@@ -51,36 +56,16 @@ defmodule Chat.ResumeSignal do
       encryption_type: encryption_type,
       encrypted: encrypted,
       signature: signature,
-      user_offset: user_offset,
-      signal_offset: signal_offset,
+      user_offset: String.to_integer(user_offset),
+      signal_offset: String.to_integer(signal_offset),
       signal_type: signal_type,
       signal_ack_state:
         Injection.get_ack_status(
-          "#{from.eid}_#{to.eid}",
-          from.connection_resource_id,
+          queue_id,
+          "",
           @partition_id,
-          signal_offset
+          String.to_integer(signal_offset)
         )
     }
   end
 end
-
-  # def build_message(
-  #   %{
-  #     id: id,
-  #     from: %{eid: from_eid, connection_resource_id: from_device_id},
-  #     to: %{eid: to_eid, connection_resource_id: to_device_id},
-  #     payload: payload,
-  #     encryption_type: encryption_type,
-  #     encrypted: encrypted,
-
-  #     signature: signature,
-  #     signal_type: signal_type,
-
-  #     user_offset: user_offset,
-  #     signal_offset: signal_offset,
-
-  #     signal_offset_state: signal_offset_state,
-  #     signal_ack_state: %{read: read, sent: sent, delivered: delivered},
-  #     timestamp:  timestamp
-  #     }) do
